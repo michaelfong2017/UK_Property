413e832996dff9eaba9949dcacde675007d9cb85
Michael Fong <michaelfong2017@gmail.com>
2021-01-07 17:43:43 +0800

    DuplicatesPipeline, gitignore

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -64,3 +64,25 @@ class SaveQuotesPipeline(object):
             session.close()
 
         return item
+
+
+class DuplicatesPipeline(object):
+
+    def __init__(self):
+        """
+        Initializes database connection and sessionmaker.
+        Creates tables.
+        """
+        engine = db_connect()
+        create_table(engine)
+        self.Session = sessionmaker(bind=engine)
+
+    def process_item(self, item, spider):
+        session = self.Session()
+        exist_quote = session.query(Quote).filter_by(quote_content = item["quote_content"]).first()
+        if exist_quote is not None:  # the current quote exists
+            raise DropItem("Duplicate item found: %s" % item["quote_content"])
+            session.close()
+        else:
+            return item
+            session.close()
--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -63,6 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
+    'tutorial.pipelines.DuplicatesPipeline': 100,
     'tutorial.pipelines.SaveQuotesPipeline': 200,
 }
 
